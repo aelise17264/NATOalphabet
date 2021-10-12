1c5c51a720a19babeb980f7286e09e856bdfbc29
Anna <aelise17264@gmail.com>
2021-10-12 16:13:48 -0600

    created project in pycharm that converts a word the user inputs into the list of NATO phonetic alphabet code words

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+#TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+alphabet_data = pandas.read_csv("nato_phonetic_alphabet.csv")
+# print(alphabet_data)
+alphabet_dict = {row.letter: row.code for (index, row) in alphabet_data.iterrows()}
+# print(alphabet_dict)
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_word = input("Enter a word: ").upper()
+user_dict = list(user_word)
+# print(user_dict)
+answer = [alphabet_dict[letter] for letter in user_dict]
+print(answer)
